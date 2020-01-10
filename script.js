c1c2ae2110f72de440bb774712203feb774d4b51
unknown <lovinmathew@hotmail.com>
2020-01-10 14:56:37 -0600

    Changed

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const passwordDisplay = document.getElementById('passwordDisplay'); 
-const form = document.getElementById('passgen');
+// const passwordDisplay = document.getElementById('passwordDisplay'); 
+// const form = document.getElementById('passgen');
+// const charList= list(33, 126);
+
+// form.addEventListener('submit', e=>{
+//     e.preventDefault();
+//     const pass = generatePass();
+//     passwordDisplay.innerText = pass;
+// })
+
+// function generatePass(){
+//     const passChar=[];
+//     for( let i=0; i<10; i++){
+//         const character=charList[Math.floor(Math.random()*charList.length)];
+//         passChar.push(String.fromCharCode(character));
+//     }
+//     return passChar.join('');
+// }
+
+// function list(low, high){
+//     const array=[];
+//     for(let i=low; i<=high; i++){
+//         array.push(i);
+//     }
+//     return array;
+// }
+
+// function btnCopy() {
+//     var change = document.getElementById("btn2");
+//     if (change.innerHTML == "Copy to Clipboard"){   
+//         change.innerHTML = "Copied !";
+//     }
+//     else {
+//         change.innerHTML = "Copy to Clipboard";
+//     }
+// } 
+
+
 const charList= list(33, 126);
-
-form.addEventListener('submit', e=>{
-    e.preventDefault();
-    const pass = generatePass();
-    passwordDisplay.innerText = pass;
-})
-
-function generatePass(){
-    const passChar=[];
-    for( let i=0; i<10; i++){
-        const character=charList[Math.floor(Math.random()*charList.length)];
-        passChar.push(String.fromCharCode(character));
-    }
-    return passChar.join('');
-}
-
 function list(low, high){
     const array=[];
     for(let i=low; i<=high; i++){
@@ -25,36 +45,18 @@ function list(low, high){
     return array;
 }
 
-function btncopy() {
-    var change = document.getElementById("btn2");
-    if (change.innerHTML == "Copy to Clipboard"){   
-        change.innerHTML = "Copied !";
-//         var aux = document.createElement("input");
+function passGen(){
+    const passLength = prompt("Password Length ?")
+    const pass = generatePass(passLength);
+    passwordDisplay.innerText = pass;
+    return false;
+}
 
-//   // Get the text from the element passed into the input
-//   aux.setAttribute("value", pass);
-
-//   // Append the aux input to the body
-//   document.body.appendChild(aux);
-
-//   // Highlight the content
-//   aux.select();
-
-//   // Execute the copy command
-//   document.execCommand("copy");
-
-//   // Remove the input from the body
-//   document.body.removeChild(aux);
+function generatePass(passLength){
+    const passChar=[];
+    for( let i=0; i<passLength; i++){
+        const character=charList[Math.floor(Math.random()*charList.length)];
+        passChar.push(String.fromCharCode(character));
     }
-    else {
-        change.innerHTML = "Copy to Clipboard";
-    }
-}    
-
-
-
-// const btn2 = document.getElementById('btn2');
-// btn2.onclick = function(){
-//      passwordDisplay.innerText.select();
-
-// }
+    return passChar.join('');
+}
